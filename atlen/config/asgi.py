f271a3adbb4cab61659189559c70991bd971ccb1
iiviie <divyanshverma158@gmail.com>
2025-01-20 02:01:30 +0530

    fix websockets not being able to communicate to each other issue

--- atlen/config/asgi.py
+++ atlen/config/asgi.py
@@ -1,44 +1,17 @@
-# ruff: noqa
-"""
-ASGI config for atlen project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/dev/howto/deployment/asgi/
-
-"""
-
 import os
-from django.core.asgi import get_asgi_application
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-
-# If DJANGO_SETTINGS_MODULE is unset, default to the local settings
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
-
-# This application object is used by any ASGI server configured to use this file.
-django_asgi_app = get_asgi_application()
-
-# Import websocket_urlpatterns after django setup
-from group_travel.routing import websocket_urlpatterns  # noqa
+from django.core.asgi import get_asgi_application
+from group_travel.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
     ),
 })
-
-# Import websocket application here, so apps from django_application are loaded first
-from config.websocket import websocket_application
-
-
-async def application(scope, receive, send):
-    if scope["type"] == "http":
-        await django_asgi_app(scope, receive, send)
-    elif scope["type"] == "websocket":
-        await websocket_application(scope, receive, send)
-    else:
-        msg = f"Unknown scope type {scope['type']}"
-        raise NotImplementedError(msg)
